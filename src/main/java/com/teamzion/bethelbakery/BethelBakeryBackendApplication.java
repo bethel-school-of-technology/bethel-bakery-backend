91dfbb59d8ec175a8f94b87da5634a7a2f112565
Tyler Estes <tjltestes@gmail.com>
2021-11-10 17:55:10 -0500

    first commit

---
+++ src/main/java/com/teamzion/bethelbakery/BethelBakeryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.teamzion.bethelbakery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BethelBakeryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BethelBakeryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamzion/bethelbakery/BethelBakeryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamzion.bethelbakery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BethelBakeryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
