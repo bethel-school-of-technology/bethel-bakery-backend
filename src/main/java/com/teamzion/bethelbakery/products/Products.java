1f9f53fa45b02ca7a2c643e4adb77ff49c45a747
Tyler Estes <tjltestes@gmail.com>
2021-11-11 01:39:23 -0500

    created the products class

---
+++ src/main/java/com/teamzion/bethelbakery/products/Products.java
@@ -0,0 +1,49 @@
+package com.teamzion.bethelbakery.products;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="products")
+public class Products {
+	
+	//Data fields 
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int product_id;
+	private String name;
+	private double price;
+	private String img_url;
+	
+	//Getters and Setters
+	public int getProduct_id() {
+		return product_id;
+	}
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getImg_url() {
+		return img_url;
+	}
+	public void setImg_url(String img_url) {
+		this.img_url = img_url;
+	}
+	
+	
+
+}
