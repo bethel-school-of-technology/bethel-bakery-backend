5b9e5366ff16fdfabc5ee84c4d7c86c15900e75e
Tyler Estes <tjltestes@gmail.com>
2021-11-16 02:49:16 -0500

    fixed crossorigin issue

--- src/main/java/com/teamzion/bethelbakery/controlers/ProductsControler.java
+++ src/main/java/com/teamzion/bethelbakery/controlers/ProductsControler.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.teamzion.bethelbakery.models.Product;
 import com.teamzion.bethelbakery.services.ProductsService;
 
+
 @RestController
 public class ProductsControler {
 
@@ -24,6 +26,7 @@ public class ProductsControler {
 	private ProductsService service;
 	
 	//Api request to get all products
+	@CrossOrigin
 	@GetMapping("/product")
 	public List<Product> getAllProducts() {
 		return service.getProducts();
